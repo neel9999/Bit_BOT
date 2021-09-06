2c74b65b1b607a8458920f8d4e7f23483674a60b
Neel <neelkanani9999@gmail.com>
2021-09-05 21:10:07 -0700

    Update KrabbyBot.py

--- KrabbyBot.py
+++ KrabbyBot.py
@@ -1,4 +1,4 @@
-#Copyright (c) 2020 kunjesh07
+#Copyright (c) 2021 NEEL18
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -18,10 +18,10 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#LICENSE   = MIT GitHub
-#__author__= KUNJESH PATEL
-#__email___= kunjesh137@gmail.com
-#__Year____= 2020
+#LICENSE   = NEEL GitHub
+#__author__= NEEL KANANI
+#__email___= neelkanani9999@gmail.com
+#__Year____= 2021
 #Version___= 1.1.0
 
 import datetime
@@ -55,13 +55,13 @@ def bot():
     responded = False
 
     if 'about' in incoming_msg or 'About' in incoming_msg:
-        text = f' 🎯🎯Created and Developed by *KUNJESH PATEL* 📌📌\n*✉️© 2020 KUNJESH PATEL.  All rights reserved.*📝 '
+        text = f'🎯Created and Developed by *NEEL KANANI*📌\n*✉️© 2021 NEEL KANANI.  All rights reserved.*📝 '
         msg.body(text)
         responded = True
 
     
     if 'start' in incoming_msg or 'Start' in incoming_msg:
-        text = f'🤖 _Hello I Am a Krabby Bot, how Can I Help You?_\n\n*Company\'s official number and website :*\n\n📞 : +917041436016\n📱 : _http://krabby.rf.gd/_ \n\n*For any query Contact on below no :*\n\n📞 : +918200398956 (Meet Savaliya )\n\n*For feedback, mail us on below mail id :*\n📧 D20DCE172@charusat.edu.in (Tejas Vaishnav)\n--------------------------------------------------------------------\n\n🚀 *Features*\n\n✅ _Covid-19 info_\n✅ _Youtube Downloader_ \n✅ _Facebook Downloader_ \n✅ _Google Search_ \n✅ _Advice_ \n✅ _wiki Search_\n✅ _weather Information_\n✅ _Quote_\n✅ _Translator_\n\n--------------------------------------------------------------------\n\n🎯 *Upcoming features* 🎯\n\n✅ _Stackoverflow query finder_\n✅ _Voice based query_\n✅ _Torrent link to Google drive link or normal link_\n✅ _Device based location in longitude and latitude_\n✅ _Instagram video Downloader_\n✅ _stocking instagram profile_\n✅ _News_\n\n--------------------------------------------------------------------\n\n_To Display Command Type_ *Menu*\n\n_To help for Command Type_ *Help or help*' 
+        text = f'🤖 _Hello I Am a Bit BOT, how Can I Help You?_\n\n*Company\'s official number and website :*\n\n📞 : +917041622101\n📱 : \n\n*For any query Contact on below no :*\n\n📞 : +917041622101 (NEEL KANANI )\n\n*For feedback, mail us on below mail id :*\n📧 neel.kanani111145@marwadiuniversity.ac.in (NEEL KANANI)\n--------------------------------------------------------------------\n\n🚀 *Features*\n\n✅ _Covid-19 info_\n✅ \n✅ \n✅ _Google Search_ \n✅ \n✅ _wiki Search_\n✅ _weather Information_\n✅ _Quote_\n✅ _Translator_\n\n--------------------------------------------------------------------\n\n🎯 *Upcoming features* 🎯\n\n✅ _Stackoverflow query finder_\n✅ _Voice based query_\n✅ _Torrent link to Google drive link or normal link_\n✅ _Device based location in longitude and latitude_\n✅ _Instagram video Downloader_\n✅ _stocking instagram profile_\n✅ _News_\n\n--------------------------------------------------------------------\n\n_To Display Command Type_ *Menu*\n\n_To help for Command Type_ *Help or help*' 
         msg.body(text)
         responded = True
 
@@ -458,7 +458,7 @@ def bot():
        responded = True
     
     if responded == False:
-        msg.body('Sorry, I Am just bot didn\'t recognized that command :), please send start to go to the menu')
+        msg.body('Sorry for inconvinience, I Am just BOT didn\'t recognized that command, please send start to go to the menu')
 
     return str(resp)
 
